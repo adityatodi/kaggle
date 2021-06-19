b1e5ade22f3058f92d62185ed675f4162162f04b
Aditya Todi <adityatodi97@gmail.com>
2021-06-19 21:15:10 +0530

    increased neural net layers

--- digit-recognizer/model.py
+++ digit-recognizer/model.py
@@ -1,7 +1,11 @@
 import pandas as pd
+from tensorflow import keras
+from keras import layers
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D, MaxPooling2D, Flatten
 from sklearn.model_selection import train_test_split
+from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.layers.experimental import preprocessing
 from keras.utils import np_utils
 import numpy as np
 import pickle
@@ -29,23 +33,35 @@ y = np_utils.to_categorical(y, n_classes)
 #%%
 X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.2, random_state=42)
 #%%
-model = Sequential()
-model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', input_shape=(28, 28, 1)))
-model.add(MaxPooling2D((2, 2)))
-model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform'))
-model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform'))
-model.add(MaxPooling2D((2, 2)))
-model.add(Flatten())
-model.add(Dense(100, activation='relu', kernel_initializer='he_uniform'))
-model.add(Dense(10, activation='softmax'))
+model = Sequential([layers.InputLayer(input_shape=[28,28,1]),
+layers.Conv2D(filters=32, kernel_size=3, activation='relu'),
+layers.BatchNormalization(axis=1),
+layers.Conv2D(filters=64, kernel_size=3, activation='relu'),
+layers.BatchNormalization(axis=1),
+layers.Conv2D(filters=128, kernel_size=5, activation='relu'),
+layers.BatchNormalization(axis=1),
+layers.Conv2D(filters=128, kernel_size=5, activation='relu'),
+layers.MaxPool2D(),
+layers.BatchNormalization(axis=1),
+layers.Flatten(),
+layers.Dense(units=1024,activation='relu'),
+layers.Dense(units=10, activation='softmax')])
 # compile model
 opt = SGD(lr=0.01, momentum=0.9)
 model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
 #%%
-model.fit(X_train, y_train, epochs=10, batch_size=32, validation_data=(X_valid, y_valid), verbose=1)
+model.fit(X_train, y_train, epochs=20, batch_size=32, validation_data=(X_valid, y_valid), verbose=1)
+#%%
+img = X_train[854]
+img1 = img.reshape(1, 28, 28, 1)
+y_expect = y_train[854]
+y_expect = np.argmax(y_expect)
+y_pred = model.predict_classes(img1)
+plt.imshow(img)
+plt.show()
 #%%
 y_pred = model.predict_classes(X_test)
 imageId = list(range(1, 28001))
 #%%
 digit_pred = pd.DataFrame(list(zip(imageId, y_pred)),columns =['ImageId', 'Label'])
-digit_pred.to_csv("digit_prediction.csv", index = False)
+digit_pred.to_csv("digit_prediction.csv", index = False)        
